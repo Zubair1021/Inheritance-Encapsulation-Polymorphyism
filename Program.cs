7ef376d1c5e03f76427b46a2bc8c31d56e274fe7
Muhammad Zubair <121050120+Zubair1021@users.noreply.github.com>
2023-06-10 02:09:14 -0700

    Concepts of OOPS piller

--- Program.cs
+++ Program.cs
@@ -4,88 +4,129 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lab8T3
+namespace lab8T2
 {
     class Program
-    {
-        public class Animal
+    { 
+        public class Person
         {
             protected string name;
-            public Animal(string name)
+            protected string address;
+
+            public Person(string name , string address)
             {
                 this.name = name;
+                this.address = address;
+            }
+            public string getname()
+            {
+                return name;
+            }
+            public string getaddress()
+            {
+                return address;
+            }
+            public void setaddress(string address)
+            {
+                this.address = address;
             }
             public virtual string tostring()
             {
-                return "Animal[ " + name +"]";
-            }
-            public virtual void greets()
-            {
-                Console.WriteLine("The Animal is Here!---");
+                return "Person[" +" Name: " + name + "\t\t" + "Address: " + address + "]";
             }
         }
-        public class mammal : Animal
+        public class Student : Person
         {
-            public mammal(string name) : base (name)
-            {
+            private string program;
+            private int year;
+            private double fee;
 
+            public Student(string name,string address,string program,int year,double fee) : base (name,address)
+            {
+                this.program = program;
+                this.year = year;
+                this.fee = fee;
+            }
+            public string getprogram()
+            {
+                return program;
+            }
+            public void setprogram(string program)
+            {
+                this.program = program;
+            }
+            public void setyear(int year)
+            {
+                this.year = year;
+            }
+            public void setfee(double fee)
+            {
+                this.fee = fee;
+            }
+            public int getyear()
+            {
+                return year;
+            }
+            public double getfee()
+            {
+                return fee;
             }
             public override string tostring()
             {
-                return "Mammal [ Animal: " + name + "]]";
+                return "STUDENT [ Person "+"Name: " + name + "\t\t" + "Address: " + address + " ]\t\t" + "Program: " + program + "\t\t" + "year: " + year + "\t\t" + "fee: " + fee +"]";
             }
-            public override void greets()
-            {
-                Console.WriteLine("The Animal is Here!---");
-            }
-        }
-        public class cat:mammal 
-        {
-            public cat(string name) : base(name)
-            {
 
+        }
+        public class staff : Person
+        {
+            private string school;
+            private double pay;
+
+            public staff(string name, string address, string school, double pay) : base(name, address)
+
+            {
+                this.school = school;
+                this.pay = pay;
+            }
+            public void setschool(string school)
+            {
+                this.school = school;
+            }
+            public string getschool()
+            {
+                return school;
+            }
+            public void setpay(double pay)
+            {
+                this.pay = pay;
+            }
+            public double getpay()
+            {
+                return pay;
             }
             public override string tostring()
             {
-                return "Cat [ Mammal [ Animal: " + name + "]]]";
+                return "Staff[ Person ["+"Name: " + name + "\t\t" + "Address: " + address + "]\t\t" + "School: " + school + "\t\t" + "Pay: " + pay+ "]";
             }
-            public override void greets()
-            {
-                Console.WriteLine("Meow");
-            }
-
         }
-        public class dog : mammal
-        {
-            public dog(string name) : base(name)
-            {
 
-            }
-            public override string tostring()
-            {
-                return "Dog[ Cat [ Mammal [ Animal: " + name + "]]]]";
-            }
-            public override void greets()
-            {
-                Console.WriteLine("Woof");
-            }
-
-
-        }
+        
         static void Main(string[] args)
         {
-            List<Animal> list = new List<Animal>();
-            cat c1 = new cat("Kitty"); list.Add(c1);
-            cat c2 = new cat("Litty"); list.Add(c2);
-            dog d1 = new dog("Tommy"); list.Add(d1);
-            dog d2 = new dog("Dogi"); list.Add(d2);
+            List<Person> list = new List<Person>();
+            Student s1 = new Student("Ali", "Lahore", "BSCS", 2, 5000); list.Add(s1);
+            Student s2 = new Student("Ahmed", "Gujrat", "BSCS", 2, 5000); list.Add(s2);
 
-            foreach(Animal a in list)
+            staff p1 = new staff("Saad", "Lahore","Peak",4300); list.Add(p1);
+            staff p2 = new staff("Hamza", "Lahore","Beachon",5000); list.Add(p2);
+
+            foreach(Person p in list)
             {
-               
-                
+                Console.WriteLine(p.tostring());
+
             }
             Console.ReadKey();
+
         }
     }
 }
